ed6412221880d8ca2e761c5ded5bf7b8775de6a1
Lee SeungJae <coex03@cau.ac.kr>
2024-06-28 16:33:46 +0900

    commit

--- optimizer.py
+++ optimizer.py
@@ -2,8 +2,6 @@ import numpy as np
 from functions import *
 
 def gradient_descent(network, X_train, y_train, batch_size, epoch, learning_rate=0.01, moment_const=0.8):
-    loss_log = []
-    train_acc_log = []
     train_size = X_train.shape[0]
     for i in range(epoch):
         batch_mask = np.random.choice(train_size, batch_size)
@@ -15,16 +13,14 @@ def gradient_descent(network, X_train, y_train, batch_size, epoch, learning_rate
             network.params[key] -= learning_rate * grad[key]
         
         loss = network.loss(X_batch, y_batch)
-        loss_log.append(loss)
+        network.loss_log.append(loss)
         
         train_acc = network.accuracy(X_train, y_train)
-        train_acc_log.append(train_acc)
+        network.train_acc_log.append(train_acc)
         
-    return loss_log, train_acc_log
+    return network.loss_log, network.train_acc_log
         
 def momentum(network, X_train, y_train, batch_size, epoch, learning_rate=0.01, moment_const=0.8):
-    loss_log = []
-    train_acc_log = []
     train_size = X_train.shape[0]
     moment = {}
     for key in list(network.params.keys()):
@@ -41,11 +37,11 @@ def momentum(network, X_train, y_train, batch_size, epoch, learning_rate=0.01, m
          network.params[key] += moment[key]
         
         loss = network.loss(X_batch, y_batch)
-        loss_log.append(loss)
+        network.loss_log.append(loss)
         train_acc = network.accuracy(X_train, y_train)
-        train_acc_log.append(train_acc)
+        network.train_acc_log.append(train_acc)
         
-    return loss_log, train_acc_log
+    return network.loss_log, network.train_acc_log
 
 optimizer_list = {
     "sgd":gradient_descent,
